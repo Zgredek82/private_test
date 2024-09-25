52eda6fbf67e20820f1f5df0394658c8c1ac5e5e
Jakub Raj <jakub.raj@lego.com>
2024-09-25 12:42:25 +0200

    my first commit oki second2

--- # write an addition of two numbers progr.py
+++ # write an addition of two numbers progr.py
@@ -9,3 +9,5 @@ num2 = float(input("Enter the second number: "))
 # Calling the add_numbers function and printing the result
 result = add_numbers(num1, num2)
 print("The sum of", num1, "and", num2, "is", result)
+
+#test comment to see if it works
